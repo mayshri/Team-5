d43d05d231d71bf231d6352c70d67d23dcab6f26
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2022-09-26 13:48:12 -0400

    Store interactions

--- src/process.py
+++ src/process.py
@@ -4,7 +4,7 @@ from typing import Union
 import pandas as pd
 
 DATAFOLDER = Path(__file__).parents[1] / "data"
-RATINGS = DATAFOLDER / "ratings.csv"
+INTERACTIONS = DATAFOLDER / "interactions.csv"
 
 
 class ProcessDumps:
@@ -33,12 +33,12 @@ class ProcessDumps:
         df["movie_id"] = df["request"].str.split("/", expand=True)[2]
         df["rating"] = df["rating"].map(lambda x: x.rstrip("'"))
         df = df.drop(labels=["request"], axis=1)
+
         return df
 
     @staticmethod
     def raw_to_interactions(raw_dump: Path) -> pd.DataFrame:
         dump_df = pd.read_csv(raw_dump, header=None)
-
         data = dump_df[6]
         df = data.str.split(",", 2, expand=True)
         df.columns = ["timestamp", "user_id", "request"]
@@ -47,20 +47,20 @@ class ProcessDumps:
         df = df.loc[df["request"].str.find("/data/") != -1]
 
         # Clean data
-        df[["request", "rating"]] = df["request"].str.split("=", expand=True)
         df["movie_id"] = df["request"].str.split("/", expand=True)[3]
         df = df.drop(labels=["request"], axis=1)
+
         return df
 
     @classmethod
     def process_new_dump(cls, raw_dump: Union[Path, str]) -> None:
 
         new_interactions = cls.raw_to_interactions(Path(raw_dump))
-        new_ratings = cls.raw_to_ratings(Path(raw_dump))
 
-        new_data = pd.concat([new_ratings, new_interactions])
-        if not RATINGS.exists():
-            new_data.to_csv(RATINGS, index=False)
+        if not INTERACTIONS.exists():
+            new_interactions.to_csv(INTERACTIONS, index=False)
 
-        combined_ratings = pd.concat([new_data, pd.read_csv(RATINGS)]).drop_duplicates()
-        combined_ratings.to_csv(RATINGS, index=False)
+        combined_ratings = pd.concat(
+            [new_interactions, pd.read_csv(INTERACTIONS)]
+        ).drop_duplicates()
+        combined_ratings.to_csv(INTERACTIONS, index=False)
---
+++ tests/test_process.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from unittest import TestCase
+
+from src.process import ProcessDumps
+
+
+class TestProcess(TestCase):
+    def test_process_raw_dump(self):
+
+        interactions = ProcessDumps.raw_to_interactions(
+            Path(__file__).parent / "test-kafka-dump.csv"
+        )
+        self.assertEqual(len(interactions), 5)
+        self.assertEqual(
+            set(interactions.columns), {"timestamp", "user_id", "movie_id"}
+        )
