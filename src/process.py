c963c1300038ca32fd678e5a0794157028842155
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2022-09-26 13:29:52 -0400

    save interactions without ratings as well

--- src/process.py
+++ src/process.py
@@ -32,17 +32,35 @@ class ProcessDumps:
         df[["request", "rating"]] = df["request"].str.split("=", expand=True)
         df["movie_id"] = df["request"].str.split("/", expand=True)[2]
         df["rating"] = df["rating"].map(lambda x: x.rstrip("'"))
-        df = df.drop(labels=["request", "timestamp"], axis=1)
+        df = df.drop(labels=["request"], axis=1)
+        return df
+
+    @staticmethod
+    def raw_to_interactions(raw_dump: Path) -> pd.DataFrame:
+        dump_df = pd.read_csv(raw_dump, header=None)
+
+        data = dump_df[6]
+        df = data.str.split(",", 2, expand=True)
+        df.columns = ["timestamp", "user_id", "request"]
+
+        # Filter by 'rating' requests
+        df = df.loc[df["request"].str.find("/data/") != -1]
+
+        # Clean data
+        df[["request", "rating"]] = df["request"].str.split("=", expand=True)
+        df["movie_id"] = df["request"].str.split("/", expand=True)[3]
+        df = df.drop(labels=["request"], axis=1)
         return df
 
     @classmethod
     def process_new_dump(cls, raw_dump: Union[Path, str]) -> None:
 
+        new_interactions = cls.raw_to_interactions(Path(raw_dump))
         new_ratings = cls.raw_to_ratings(Path(raw_dump))
-        if not RATINGS.exists():
-            new_ratings.to_csv(RATINGS, index=False)
 
-        combined_ratings = pd.concat(
-            [new_ratings, pd.read_csv(RATINGS)]
-        ).drop_duplicates()
+        new_data = pd.concat([new_ratings, new_interactions])
+        if not RATINGS.exists():
+            new_data.to_csv(RATINGS, index=False)
+
+        combined_ratings = pd.concat([new_data, pd.read_csv(RATINGS)]).drop_duplicates()
         combined_ratings.to_csv(RATINGS, index=False)
