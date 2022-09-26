fb36de30f5bb5fb6448269f3ca55da8f432467fe
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2022-09-26 13:57:50 -0400

    Fix failing tests

--- scripts/dump.py
+++ scripts/dump.py
@@ -2,17 +2,18 @@ import csv
 
 from kafka import KafkaConsumer
 
-server = 'fall2022-comp585.cs.mcgill.ca:9092'
-topic = 'movielog5'
+server = "fall2022-comp585.cs.mcgill.ca:9092"
+topic = "movielog5"
 
-consumer = KafkaConsumer(topic, bootstrap_servers=[server], api_version=(0,11,5))
+consumer = KafkaConsumer(topic, bootstrap_servers=[server], api_version=(0, 11, 5))
 
-f = open('data.csv', 'w')
+f = open("data.csv", "w")
 writer = csv.writer(f)
 
 num = 0
 for message in consumer:
-    if num >= 100000: break
+    if num >= 100000:
+        break
 
     num += 1
     writer.writerow(message)
