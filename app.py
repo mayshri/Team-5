f051cc25e972740872edf68a957c1b4709594db4
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2022-09-27 08:19:58 -0400

    Fix failing tests

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask,request
-from src.dummyModel import *
 import requests
+from flask import Flask
+
+from src.dummyModel import inference
+
 app = Flask(__name__)
 
-@app.route('/recommend/<userid>')
-def recommand(userid:str):
-    url = 'http://fall2022-comp585.cs.mcgill.ca:8080/user/'+userid
+
+@app.route("/recommend/<userid>")
+def recommand(userid: str):
+    url = "http://fall2022-comp585.cs.mcgill.ca:8080/user/" + userid
     respond = requests.get(url=url)
-    r=inference(respond.json())
+    r = inference(respond.json())
     return r
