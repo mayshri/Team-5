85b88cb77b7b7cd14b6043c4b31bd89169314c1d
Eggie-Wu <qihan.wu@mail.mcgill.ca>
2022-10-01 17:21:26 -0400

    update deployment

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@
 
 FROM python:3.8-slim-buster
 
+RUN apt-get -y update
+RUN apt-get -y install git
+
+
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
@@ -10,4 +14,5 @@ RUN pip3 install -r requirements.txt
 COPY . .
 
 CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
+
 
--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import requests
 from flask import Flask
-
-from src.dummyModel import inference
-
+from src.model import Model
+import json
+import requests
 app = Flask(__name__)
 
 
 @app.route("/recommend/<userid>")
-def recommand(userid: str):
-    url = "http://fall2022-comp585.cs.mcgill.ca:8080/user/" + userid
-    respond = requests.get(url=url)
-    r = inference(respond.json())
-    return r
+def response(userid: str):
+    r = Model.recommend(int(userid))
+    result={}
+    for i in range(20):
+        url = "http://fall2022-comp585.cs.mcgill.ca:8080/movie/" + r[i]
+        result.update({i+1:requests.get(url=url).json()})
+    return json.dumps(result)
