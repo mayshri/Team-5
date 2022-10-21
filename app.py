5012e271d040686d1d37d687358f68bee15b5389
Eggie-Wu <qihan.wu@mail.mcgill.ca>
2022-10-21 12:01:57 -0400

    fix return format

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import json
-
-import requests
 from flask import Flask
 
 from src.model import Model
@@ -11,8 +8,9 @@ app = Flask(__name__)
 @app.route("/recommend/<userid>")
 def response(userid: str):
     r = Model.recommend(int(userid))
-    result = {}
+    result=""
     for i in range(20):
-        url = "http://fall2022-comp585.cs.mcgill.ca:8080/movie/" + r[i]
-        result.update({i + 1: requests.get(url=url).json()})
-    return json.dumps(result)
+        result+=r[i]
+        result+=","
+    result=result[:-1]
+    return result
