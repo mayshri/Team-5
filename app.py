8c5859e0e0cc28d9b4a23b94489f42058a83f43b
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2022-10-21 12:20:13 -0400

    Apply black formatting to app.py

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 @app.route("/recommend/<userid>")
 def response(userid: str):
     r = Model.recommend(int(userid))
-    result=""
+    result = ""
     for i in range(20):
-        result+=r[i]
-        result+=","
-    result=result[:-1]
+        result += r[i]
+        result += ","
+    result = result[:-1]
     return result
