ee113a5ea1b802d49fd82cc18f88cd53c8a24d4c
Arthur <monnierarthur@gmail.com>
2022-10-25 18:03:33 -0400

    Fix code quality

--- src/model.py
+++ src/model.py
@@ -61,53 +61,38 @@ class Model:
         self.top_20 = list(self.interactions["movie_id"].value_counts().head(20).index)
         self.users = set(self.interactions["user_id"].unique())
 
-        self.recommendations = []
+        self.recommendations = {}
 
         self.recommmended_movies_watched = 0
         self.total_recommendations = 0
 
         self.recommmended_movies_positive_rating = 0
         self.total_recommendations_rated = 0
-
         self.setup_online_testing()
 
     def parse_entry(self, entry):
         parsed = entry.value.decode().split(",")
 
-        timestamp = parsed[0]
-        used_id = parsed[1]
-
-        # Write the metrics to a file
-        if self.total_recommendations % 1000 == 0:
-            with open(config.RECOMMENDEDMOVIEWATCHRATE, 'w') as f:
-                print('writing watch rate', str(self.compute_recommendation_watch_rate()))
-                f.write(str(self.compute_recommendation_watch_rate()))
-
-            with open(config.RECOMMENDEDMOVIEACCURACY, 'w') as f:
-                print('writing accuracy', str(self.compute_recommendation_accuracy()))
-                f.write(str(self.compute_recommendation_accuracy()))
+        # timestamp = parsed[0]
+        user_id = parsed[1]
         
         # If length is <= 3 then the request is either a /data/ or /rate/ request
         if len(parsed) <= 3:
-            user_recommendations = [reco for reco in self.recommendations if reco[1] == used_id] # Get all recommendations for this user
+            user_recommendations = self.recommendations.get(user_id) # Get all recommendations for this user
             
+            # If the user has no recommendations, then we return as we don't make use of this data
+            if user_recommendations is None: return
 
             # We get the type of request 
             # (is is_data_request = true then it is a /data/ request else it is a /rate/ request)
             is_data_request = parsed[2].find("/data/") != -1
              # we get the movie id from the request
-
-            # If the user has no recommendations, then we return as we don't make use of this data
-            if len(user_recommendations) <= 0:
-                return
             
             # If it is a /data/ request, we want to compute the "Recommended Movie Watch" Rate
             if is_data_request: 
                 movie_id = parsed[2].split("/")[3]
-                for reco in user_recommendations:
-                    if movie_id in reco[2]:
-                        self.recommmended_movies_watched += 1
-                    self.recommendations.remove(reco)
+                if movie_id in user_recommendations:
+                    self.recommmended_movies_watched += 1
                 return
 
             # If it is a /rate/ request, we want to compute the "Recommendation Accuracy" Rate
@@ -116,27 +101,38 @@ class Model:
                 rating = movie_id.split("=")[1]
                 movie_id = movie_id.split("=")[0]
 
-                for reco in user_recommendations:
-                    if movie_id in reco[2]:
-                        self.total_recommendations_rated += 1
-                        if rating >= 5:
-                            self.recommmended_movies_positive_rating += 1
-                    self.recommendations.remove(reco)
+                if movie_id in user_recommendations:
+                    self.total_recommendations_rated += 1
+                    if float(rating) >= 5:
+                        self.recommmended_movies_positive_rating += 1
+                self.recommendations.pop(user_id)
                 return
         
         # Parse the movies so we only get the movies id
         movies_recommended = parsed[4:24]
         movies_recommended[0] = movies_recommended[0].replace("result: ", "")
         movies_recommended = [s.strip() for s in movies_recommended]
-        self.recommendations.append((timestamp, used_id, movies_recommended))
+        self.recommendations[user_id] = movies_recommended
         self.total_recommendations += 1
+
+    def write_metrics(self):
+        # Write the metrics to a file
+        with open(config.RECOMMENDEDMOVIEWATCHRATE, 'w') as f:
+            print('writing watch rate', str(self.compute_recommendation_watch_rate()))
+            f.write(str(self.compute_recommendation_watch_rate()))
+
+        with open(config.RECOMMENDEDMOVIEACCURACY, 'w') as f:
+            print('writing accuracy', str(self.compute_recommendation_accuracy()))
+            f.write(str(self.compute_recommendation_accuracy()))
 
 
     def compute_recommendation_watch_rate(self):
+        print(self.recommmended_movies_watched, self.total_recommendations)
         if self.total_recommendations == 0: return 0
         return self.recommmended_movies_watched / self.total_recommendations
 
     def compute_recommendation_accuracy(self):
+        print(self.recommmended_movies_positive_rating, self.total_recommendations_rated)
         if self.total_recommendations_rated == 0: return 0
         return self.recommmended_movies_positive_rating / self.total_recommendations_rated
 
@@ -146,8 +142,13 @@ class Model:
 
         consumer = KafkaConsumer(topic, bootstrap_servers=[server], api_version=(0, 11, 5))
 
+        entries = 0
         for message in consumer:
+            entries += 1
             self.parse_entry(message)
+            if entries == 10000:
+                entries = 0
+                self.write_metrics()
 
     def load_model(self):
         self.model = torch.load(config.MODEL)
