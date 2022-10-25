bf333052f901f81bb97440b7c0c454e040eeb96e
Arthur <monnierarthur@gmail.com>
2022-10-24 23:14:33 -0400

    Implement Recommendation Movie Watch Rate and Compute Recommendation Accuracy metrics

--- src/model.py
+++ src/model.py
@@ -61,10 +61,84 @@ class Model:
         self.top_20 = list(self.interactions["movie_id"].value_counts().head(20).index)
         self.users = set(self.interactions["user_id"].unique())
 
+        self.recommendations = []
+
+        self.recommmended_movies_watched = 0
+        self.total_recommendations = 0
+
+        self.recommmended_movies_positive_rating = 0
+        self.total_recommendations_rated = 0
+
         self.setup_online_testing()
 
     def parse_entry(self, entry):
-        print(entry)
+        parsed = entry.value.decode().split(",")
+
+        timestamp = parsed[0]
+        used_id = parsed[1]
+
+        # Write the metrics to a file
+        if self.total_recommendations % 1000 == 0:
+            with open(config.RECOMMENDEDMOVIEWATCHRATE, 'w') as f:
+                print('writing watch rate', str(self.compute_recommendation_watch_rate()))
+                f.write(str(self.compute_recommendation_watch_rate()))
+
+            with open(config.RECOMMENDEDMOVIEACCURACY, 'w') as f:
+                print('writing accuracy', str(self.compute_recommendation_accuracy()))
+                f.write(str(self.compute_recommendation_accuracy()))
+        
+        # If length is <= 3 then the request is either a /data/ or /rate/ request
+        if len(parsed) <= 3:
+            user_recommendations = [reco for reco in self.recommendations if reco[1] == used_id] # Get all recommendations for this user
+            
+
+            # We get the type of request 
+            # (is is_data_request = true then it is a /data/ request else it is a /rate/ request)
+            is_data_request = parsed[2].find("/data/") != -1
+             # we get the movie id from the request
+
+            # If the user has no recommendations, then we return as we don't make use of this data
+            if len(user_recommendations) <= 0:
+                return
+            
+            # If it is a /data/ request, we want to compute the "Recommended Movie Watch" Rate
+            if is_data_request: 
+                movie_id = parsed[2].split("/")[3]
+                for reco in user_recommendations:
+                    if movie_id in reco[2]:
+                        self.recommmended_movies_watched += 1
+                    self.recommendations.remove(reco)
+                return
+
+            # If it is a /rate/ request, we want to compute the "Recommendation Accuracy" Rate
+            if not is_data_request: 
+                movie_id = parsed[2].split("/rate/")[1]
+                rating = movie_id.split("=")[1]
+                movie_id = movie_id.split("=")[0]
+
+                for reco in user_recommendations:
+                    if movie_id in reco[2]:
+                        self.total_recommendations_rated += 1
+                        if rating >= 5:
+                            self.recommmended_movies_positive_rating += 1
+                    self.recommendations.remove(reco)
+                return
+        
+        # Parse the movies so we only get the movies id
+        movies_recommended = parsed[4:24]
+        movies_recommended[0] = movies_recommended[0].replace("result: ", "")
+        movies_recommended = [s.strip() for s in movies_recommended]
+        self.recommendations.append((timestamp, used_id, movies_recommended))
+        self.total_recommendations += 1
+
+
+    def compute_recommendation_watch_rate(self):
+        if self.total_recommendations == 0: return 0
+        return self.recommmended_movies_watched / self.total_recommendations
+
+    def compute_recommendation_accuracy(self):
+        if self.total_recommendations_rated == 0: return 0
+        return self.recommmended_movies_positive_rating / self.total_recommendations_rated
 
     def setup_online_testing(self):
         server = "fall2022-comp585.cs.mcgill.ca:9092"
@@ -107,7 +181,6 @@ class Model:
         best_movie_id_indices = indices[np.argsort(pred[indices])]
 
         movies_recommended = [self.get_movie_id(movie) for movie in best_movie_id_indices]
-        self.setup_online_testing(movies_recommended)
         return movies_recommended
 
     def recommend(self, user_id):
