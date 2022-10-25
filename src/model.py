ffcbc8919949817d13df3415e57969deaa76b431
Arthur <monnierarthur@gmail.com>
2022-10-25 19:14:32 -0400

    Use black to fix format

--- src/model.py
+++ src/model.py
@@ -75,22 +75,25 @@ class Model:
 
         # timestamp = parsed[0]
         user_id = parsed[1]
-        
+
         # If length is <= 3 then the request is either a /data/ or /rate/ request
         if len(parsed) <= 3:
-            user_recommendations = self.recommendations.get(user_id) # Get all recommendations for this user
-            
-            # If the user has no recommendations, then we return as we don't make use of this data
-            if user_recommendations is None: return
+            user_recommendations = self.recommendations.get(
+                user_id
+            )  # Get all recommendations for this user
 
-            # We get the type of request 
+            # If the user has no recommendations, then we return as we don't make use of this data
+            if user_recommendations is None:
+                return
+
+            # We get the type of request
             # (is is_data_request = true then it is a /data/ request else it is a /rate/ request)
             is_data_request = parsed[2].find("/data/") != -1
             is_rating_request = parsed[2].find("/rate/") != -1
-             # we get the movie id from the request
-            
+            # we get the movie id from the request
+
             # If it is a /data/ request, we want to compute the "Recommended Movie Watch" Rate
-            if is_data_request: 
+            if is_data_request:
                 movie_id = parsed[2].split("/")[3]
                 if movie_id in user_recommendations:
                     self.recommmended_movies_watched += 1
@@ -109,8 +112,9 @@ class Model:
                         self.recommmended_movies_positive_rating += 1
                 self.recommendations.pop(user_id)
                 return
-            else: return
-        
+            else:
+                return
+
         # Parse the movies so we only get the movies id
         movies_recommended = parsed[4:24]
         movies_recommended[0] = movies_recommended[0].replace("result: ", "")
@@ -120,30 +124,37 @@ class Model:
 
     def write_metrics(self):
         # Write the metrics to a file
-        with open(config.RECOMMENDEDMOVIEWATCHRATE, 'w') as f:
-            print('writing watch rate', str(self.compute_recommendation_watch_rate()))
+        with open(config.RECOMMENDEDMOVIEWATCHRATE, "w") as f:
+            print("writing watch rate", str(self.compute_recommendation_watch_rate()))
             f.write(str(self.compute_recommendation_watch_rate()))
 
-        with open(config.RECOMMENDEDMOVIEACCURACY, 'w') as f:
-            print('writing accuracy', str(self.compute_recommendation_accuracy()))
+        with open(config.RECOMMENDEDMOVIEACCURACY, "w") as f:
+            print("writing accuracy", str(self.compute_recommendation_accuracy()))
             f.write(str(self.compute_recommendation_accuracy()))
-
 
     def compute_recommendation_watch_rate(self):
         print(self.recommmended_movies_watched, self.total_recommendations)
-        if self.total_recommendations == 0: return 0
+        if self.total_recommendations == 0:
+            return 0
         return self.recommmended_movies_watched / self.total_recommendations
 
     def compute_recommendation_accuracy(self):
-        print(self.recommmended_movies_positive_rating, self.total_recommendations_rated)
-        if self.total_recommendations_rated == 0: return 0
-        return self.recommmended_movies_positive_rating / self.total_recommendations_rated
+        print(
+            self.recommmended_movies_positive_rating, self.total_recommendations_rated
+        )
+        if self.total_recommendations_rated == 0:
+            return 0
+        return (
+            self.recommmended_movies_positive_rating / self.total_recommendations_rated
+        )
 
     def setup_online_testing(self):
         server = "fall2022-comp585.cs.mcgill.ca:9092"
         topic = "movielog5"
 
-        consumer = KafkaConsumer(topic, bootstrap_servers=[server], api_version=(0, 11, 5))
+        consumer = KafkaConsumer(
+            topic, bootstrap_servers=[server], api_version=(0, 11, 5)
+        )
 
         entries = 0
         for message in consumer:
