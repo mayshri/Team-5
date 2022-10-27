cef4d9beccdc1799329aa505598c9825d84e6e3c
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2022-10-27 12:06:55 -0400

    Add test to ensure model can be smoothly trained

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,7 +6,7 @@ from src.model import Model
 
 
 class TestModel(TestCase):
-    def test_model_evaluation(self):
+    def test_pretrained_model_evaluation(self):
 
         model = Model()
         model.load_model()
@@ -19,3 +19,14 @@ class TestModel(TestCase):
         print(f"\n Model MRR score STD: {np.std(mrr_scores)} \n")
         print(f"\n Model best MRR score: {max(mrr_scores)} \n")
         print(f"\n Model worst MRR score: {min(mrr_scores)} \n")
+
+    def test_model_training_and_evaluation(self):
+        model = Model()
+
+        train, test = model.load_interactions()
+        model.fit(train)
+        mrr_scores = model.eval(test)
+
+        # let's make sure the watched movie is in the top
+        # 100 movies on average (the top 0.7% of all movies)
+        self.assertGreater(np.mean(mrr_scores), 0.001)
