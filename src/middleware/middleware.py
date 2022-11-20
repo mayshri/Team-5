7b84783c1b8bfa46b8ca92b1229f1a76c814afe7
Eggie-Wu <qihan.wu@mail.mcgill.ca>
2022-11-20 11:33:14 -0500

    modify after testing on server

--- src/middleware/middleware.py
+++ src/middleware/middleware.py
@@ -8,8 +8,22 @@ app = Flask(__name__)
 @app.route("/recommend/<userid>")
 def response(userid: str):
     try:
-        reply = ask_inference(userid).content
-        return reply
+        reply = ask_inference(userid)
+        if reply.status_code == 200:
+            return reply.content
+        else:
+            return (
+                "the+shawshank+redemption+1994,interstellar+2014,"
+                "the+lord+of+the+rings+the+fellowship+of+the+ring+2001,inception+2010,"
+                "the+lord+of+the+rings+the+two+towers+2002,"
+                "the+lord+of+the+rings+the+return+of+the+king+2003,"
+                "the+godfather+1972,star+wars+1977,pulp+fiction+1994,"
+                "the+dark+knight+2008,the+green+mile+1999,"
+                "spirited+away+2001,the+avengers+2012,seven+samurai+1954,the+matrix+1999,"
+                "harry+potter+and+the+deathly+hallows+part+1+2010,"
+                "the+dark+knight+rises+2012,fight+club+1999,"
+                "howls+moving+castle+2004,whisper+of+the+heart+1995 "
+            )
     except Exception:
         # send email here
         return (
