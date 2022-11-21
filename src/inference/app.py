a7f3f184a33e643fbde9789de4828e1668bbd9f9
StevenYuan666 <ye.yuan3@mail.mcgill.ca>
2022-11-21 15:15:55 -0500

    Revert "Setup the user group for canary model and live model"
    This reverts commit ef61a5d2b1b2a7004e4b66ae409d41f30ef491a9.

--- src/inference/app.py
+++ src/inference/app.py
@@ -9,9 +9,6 @@ app = Flask(__name__)
 live_model = Model(config.LIVE_MODEL)
 canary_model = Model(config.CANARY_MODEL)
 
-# 20% users will be assigned for testing the new model
-user_class_round = [[0, 5], [1, 6], [2, 7], [3, 8], [4, 9]]
-canary_round = 0
 
 cron = Scheduler(daemon=True)
 # Explicitly kick off the background thread
@@ -36,8 +33,7 @@ def metric():
 
 @app.route("/recommend/<userid>")
 def response(userid: str):
-    # this bring more fairness
-    if int(userid[-1]) in user_class_round[canary_round % 5]:
+    if int(userid[-1]) <= 2:
         try:
             return canary_model.recommend(int(userid))
         except Exception:
